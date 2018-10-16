e680d5ad158ea4e7d5794b0c914c1957b6b57339
万能的N/P硅 <npofsi@outlook.com>
2018-10-16 21:00:38 +0800

    Update main.cpp

--- lg1056/main.cpp
+++ lg1056/main.cpp
@@ -2,19 +2,113 @@
 #include <algorithm>
 #include <vector>
 using namespace std;
-typedef struct {
-  int x, y;
-  int p, q;
-}group;
+
+typedef struct group {
+	int l;
+	int x, y;
+	int p, q;
+} ;
+int a[1000];
+
+int M, N, K, L, D;
+
 vector<group> vx, vy;
+
+int cx[1000],cy[1000];
+
+int sx[1000],sy[1000];
+
+int maxX() {
+	int maxxl=0;
+	for(int i=0; i<	M; i++) {
+		if(cx[i]>cx[maxxl]&&i!=-1)maxxl=i;
+	}
+	cx[maxxl]=-1;
+	return maxxl;
+}
+int maxY() {
+	int maxyl=0;
+	for(int i=0; i<N; i++) {
+		if(cy[i]>cy[maxyl]&&i!=-1)maxyl=i;
+	}
+	cy[maxyl]=-1;
+	return maxyl;
+}
 int main() {
-  int M, N, K, L, D;
-  cin >> M >> N >> K >> L >> D;
-  for (int i = 0; i < D; i++) {
-    group g;
-    cin >> g.x >> g.y >> g.p >> g.q;
-    if (g.x == g.p)vy.push_back(g);
-    if (g.y == g.q)vx.push_back(g);
-  }
-return 0;
- }
+
+	cin >> M >> N >> K >> L >> D;
+	for (int i = 0; i < D; i++) {
+		group g;
+		cin >> g.x >> g.y >> g.p >> g.q;
+		if (g.x == g.p) {
+			g.l=(g.y+g.q)/2;
+			vy.push_back(g);
+
+		} else if (g.y == g.q) {
+			g.l=(g.x+g.p)/2;
+			vx.push_back(g);
+		}
+	}
+
+	for(int i=0; i<vx.size(); i++) {
+		cx[vx[i].l]++;
+	}
+
+	for(int i=0; i<vy.size(); i++) {
+		cy[vy[i].l]++;
+	}
+
+	for(int i=0; i<K; i++) {
+		sx[i]=maxX();
+	}
+
+	for(int i=0; i<L; i++) {
+		sy[i]=maxY();
+	}
+	
+	sort(sx,sx+K);
+	sort(sy,sy+L);
+	
+	for(int i=0; i<K; i++) {
+		cout<<sx[i]<<' ';
+	}
+	
+	cout<<endl;
+	
+	for(int i=0; i<L; i++) {
+		cout<<sy[i]<<' ';
+	}
+	
+	
+	
+	
+	return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
