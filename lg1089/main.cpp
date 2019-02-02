7c3954ed0a34cebcddbf32b2dc083e12819f3699
npofsi <npofsi@github.com>
2019-02-02 22:47:27 +0800

    ggs ddu

--- lg1089/main.cpp
+++ lg1089/main.cpp
@@ -6,13 +6,31 @@
 using namespace std;
 
 int red[13];
-
+int inhand=0;
+int now=0;
 int main(){
 	
 	for(int i=1;i<=12;i++){
 		scanf("%d",&red[i]);
+		now+=300;
+		
+		if((now-red[i])>=100){
+								
+				
+				//cout<<"*"<<now<<endl;
+				inhand+=((now-red[i])/100)*100;
+		}else{
+			if((now-red[i])<0){
+					  cout<<"-"<<i;
+					  return 0;	
+			}
+			
+		}
+	now=(now-red[i])%100;
 	};
 	
+	
+	printf("%.0f",now+inhand*(1.2));
 	
 	
 	return 0;
