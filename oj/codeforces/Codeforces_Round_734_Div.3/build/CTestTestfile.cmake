8843cf1dc04b200de512ae03d05e40f4c49ce867
N/P硅 <npofsi@outlook.com>
2021-08-16 21:55:43 +0800

    update

--- oj/codeforces/Codeforces_Round_734_Div.3/build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Left/Workspace/Study/oj/codeforces/Codeforces_Round_734_Div.3
-# Build directory: C:/Left/Workspace/Study/oj/codeforces/Codeforces_Round_734_Div.3/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
