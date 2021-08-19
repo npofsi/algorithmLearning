f917b9df00f368427a2e1664ab172370e72cb31e
N/P硅 <npofsi@outlook.com>
2021-08-19 11:16:36 +0800

    update

---
+++ oj/vjudge/xszds03/A.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+
+using namespace std;
+
+int main() {
+	cout << "Hello A" << endl;
+	return 0;
+}
---
+++ oj/vjudge/xszds03/C.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+
+using namespace std;
+
+int main() {
+	cout << "Hello C" << endl;
+	return 0;
+}
